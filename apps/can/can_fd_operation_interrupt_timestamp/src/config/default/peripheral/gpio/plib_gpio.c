cfe5450e3e7a750f1196418f69db6ee38c025b0c
Varun A V <varun.av@microchip.com>
2024-10-16 15:32:05 +0530

    [Apps] Regenerated and tested can_fd_interrupt_timestamp application
    MH3-102767

--- apps/can/can_fd_operation_interrupt_timestamp/src/config/default/peripheral/gpio/plib_gpio.c
+++ apps/can/can_fd_operation_interrupt_timestamp/src/config/default/peripheral/gpio/plib_gpio.c
@@ -58,6 +58,8 @@
 */
 void GPIO_Initialize ( void )
 {
+    /* Disable JTAG since at least one of its pins is configured for Non-JTAG function */
+    CFG_REGS->CFG_CFGCON0CLR = CFG_CFGCON0_JTAGEN_Msk;
     /* PORTA Initialization */
     /* PORTB Initialization */
     GPIOB_REGS->GPIO_ANSELCLR = 0xcU; /* Digital Mode Enable */
@@ -67,6 +69,7 @@ void GPIO_Initialize ( void )
 
 
     /* PPS Input Remapping */
+    PPS_REGS->PPS_QD2R = 24U;
     PPS_REGS->PPS_CAN1RXR = 1U;
 
     /* PPS Output Remapping */
