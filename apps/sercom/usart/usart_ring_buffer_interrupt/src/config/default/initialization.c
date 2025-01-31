fb9c334bdd8c6c2ab8ad73beaa5b23eb8b4e1374
Vishal Gandhi <vishal.gandhi@microchip.com>
2025-01-31 12:25:16 +0530

    [SERCOM] Regenerated demo for sercom__usart__usart_ring_buffer_interrupt__pic32wm_bz6_curiosity.X
    MH3-109046

--- apps/sercom/usart/usart_ring_buffer_interrupt/src/config/default/initialization.c
+++ apps/sercom/usart/usart_ring_buffer_interrupt/src/config/default/initialization.c
@@ -215,9 +215,9 @@ void SYS_Initialize ( void* data )
 
 	GPIO_Initialize();
 
-    SERCOM0_USART_Initialize();
-
     EVSYS_Initialize();
+
+    SERCOM0_USART_Initialize();
 
 
     NVIC_Initialize();
