b4da310e3048a29da6e84f54ee06efbf2f23735d
Syam Nair <Syam.Nair@microchip.com>
2025-09-16 17:46:39 +0000

    [APP] Regenerated demo for sercom__spi__spi_eeprom_write_read
    MH3C-269

--- apps/sercom/spi/spi_eeprom_write_read/src/config/default/peripheral/sercom/spi_master/plib_sercom3_spi_master.c
+++ apps/sercom/spi/spi_eeprom_write_read/src/config/default/peripheral/sercom/spi_master/plib_sercom3_spi_master.c
@@ -62,7 +62,7 @@
 #define SERCOM3_SPIM_BAUD_VALUE         (63UL)
 
 /*Global object to save SPI Exchange related data  */
-volatile static SPI_OBJECT sercom3SPIObj;
+static volatile SPI_OBJECT sercom3SPIObj;
 
 // *****************************************************************************
 // *****************************************************************************
